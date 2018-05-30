b4c4d53659941333db13460892415a61b1e9026c
Piotr Pawlaczyk <piotrpawlaczyk21@gmail.com>
2018-05-30 12:43:28 +0200

    v 1.1 add: layouts

---
+++ app/src/main/java/com/example/piotr/bricklist/ProjectSettings.kt
@@ -0,0 +1,12 @@
+package com.example.piotr.bricklist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ProjectSettings : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_project_settings)
+    }
+}
